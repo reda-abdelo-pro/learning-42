e1fffe96f146d98e51f730b03cd5c741ad0773fb
reda-abdelo-pro <nesemoq@gmail.com>
2026-09-16 13:51:16 +0200

    sorry du retard

---
+++ ex01/print_groups.sh
@@ -0,0 +1,3 @@
+ groups $FT_USER | tr ' ' ','
+
+Groups est la commande linux qui affiche les groupes  puis $ prend la valeur de la variable FT USER | tr sert a remplacer le espace par la virgule.
---
+++ ex02/find_sh.sh
@@ -0,0 +1,2 @@
+find .iname '.sh*' -exec basename {} .sh \;
+-exec sert a executer la commande basename qui sert  a afficher le nom du dossier sans le .sh
